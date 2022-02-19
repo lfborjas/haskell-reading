95c38e0a4c938fbef042eac955bcda3a1c9a7931
Luis Borjas Reyes <luisfborjas@gmail.com>
2022-02-19 16:19:45 -0500

    Stage 1: handling IO exceptions
    Also, eitherToErr to only handle errors
    as exceptions, not Either + exceptions

--- effective-haskell/hcat/src/HCat.hs
+++ effective-haskell/hcat/src/HCat.hs
@@ -1,17 +1,28 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE LambdaCase #-}
 module HCat where
 
 import qualified System.Environment as Env
+import qualified Control.Exception as Exception
+import qualified System.IO.Error as IOError
 
 runHCat :: IO ()
 runHCat =
-  handleArgs >>= displayMessage
+  -- NOTE(luis) a @withErrorHandling@ is used
+  -- in the book vs. the infix version of 'catch'
+  (handleArgs 
+  >>= eitherToErr
+  >>= readFile
+  >>= putStrLn) `Exception.catch` handleErr
   where
-    displayMessage parsedArgument =
-      case parsedArgument of
-        Left errMessage ->
-          putStrLn $ "Error: " <> errMessage
-        Right filename ->
-          putStrLn $ "Opening file: " <> filename
+    handleErr :: IOError -> IO ()
+    handleErr e = putStrLn "Error!" >> print @IOError e
+    
+-- using throwIO vs. throw since it would otherwise be
+-- "unpredictable" when running in IO
+eitherToErr :: Show a => Either a b -> IO b
+eitherToErr (Right a) = pure a
+eitherToErr (Left e)  = Exception.throwIO . IOError.userError $ show e
 
 handleArgs :: IO (Either String FilePath)
 handleArgs =
