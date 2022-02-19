ff41fff235c81ef0bbee8a86ffcb547e711721bd
Luis Borjas Reyes <luisfborjas@gmail.com>
2022-02-19 17:38:11 -0500

    Stage 2: simple pager

--- effective-haskell/hcat/src/HCat.hs
+++ effective-haskell/hcat/src/HCat.hs
@@ -8,11 +8,19 @@ import qualified System.IO.Error as IOError
 import qualified Data.Text as Text
 import qualified Data.Text.IO as TextIO
 import qualified Data.ByteString as BS
+import qualified System.Info
+import qualified System.Process as Process
+import System.IO
 
 data ScreenDimensions = ScreenDimensions
   { screenRows :: Int
   , screenColumns :: Int
   } deriving Show
+
+data ContinueCancel
+  = Continue
+  | Cancel
+  deriving (Eq, Show)
 
 runHCat :: IO ()
 runHCat =
@@ -20,8 +28,13 @@ runHCat =
   -- in the book vs. the infix version of 'catch'
   (handleArgs 
   >>= eitherToErr
-  >>= TextIO.readFile
-  >>= TextIO.putStrLn) `Exception.catch` handleErr
+  >>= flip openFile ReadMode
+  >>= TextIO.hGetContents
+  >>= \contents ->
+    getTerminalSize >>= \termSize ->
+      let pages = paginate termSize contents
+      in showPages pages
+  ) `Exception.catch` handleErr
   where
     handleErr :: IOError -> IO ()
     handleErr e = putStrLn "Error!" >> print @IOError e
@@ -41,6 +54,32 @@ handleArgs =
         [fname] -> Right fname
         []      -> Left "No filename provided"
         _       -> Left "Multiple files not supported"
+
+getContinue :: IO ContinueCancel
+getContinue =
+  -- disable line buffering: otherwise we wouldn't get input until a newline is introduced
+  hSetBuffering stdin NoBuffering
+  -- disable echoing so user input is not echoed back in the terminal
+  >> hSetEcho stdin False
+  >> hGetChar stdin
+  >>= \case
+    ' ' -> return Continue
+    'q' -> return Cancel
+    _   -> getContinue
+
+showPages :: [Text.Text] -> IO ()
+showPages [] = return ()
+showPages (page:pages) =
+  clearScreen
+  >> TextIO.putStrLn page
+  >> getContinue
+  >>= \case
+      Continue -> showPages pages
+      Cancel   -> return ()
+
+-- TODO: look up this crazy escape seq
+clearScreen :: IO ()
+clearScreen = BS.putStr "\^[[1J\^[[1;1H"
 
 -- >>> groupsOf 3 [1,2,3,4,5,6,7]
 -- [[1,2,3],[4,5,6],[7]]
@@ -79,3 +118,21 @@ paginate (ScreenDimensions rows cols) text =
       wrappedLines   = concatMap (wordWrap cols) unwrappedLines
       pageLines      = groupsOf rows wrappedLines
   in map Text.unlines pageLines
+
+-- | System-dependent dimension deviser -- uses `tput`
+getTerminalSize :: IO ScreenDimensions
+getTerminalSize =
+  case System.Info.os of
+    "darwin" -> tputScreenDimensions
+    "linux"  -> tputScreenDimensions
+    _other   -> pure $ ScreenDimensions 25 80
+  where
+    tputScreenDimensions :: IO ScreenDimensions
+    tputScreenDimensions =
+      Process.readProcess "tput" ["lines"] ""
+      >>= \linien ->
+        Process.readProcess "tput" ["cols"] ""
+        >>= \cols ->
+          let lines' = read . init $ linien
+              cols'  = read . init $ cols
+          in return $ ScreenDimensions lines' cols'
